0f53d0c07a293847e71b0ba2fb9a289b0cdad8f1
ildunari <kosta963@gmail.com>
2025-09-04 21:01:08 -0400

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Kosta Milovanovic on 9/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ChatAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
